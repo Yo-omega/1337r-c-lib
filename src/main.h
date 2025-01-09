12056383351dac00336a6b0957b07e8a05625615
El bouzidi Youssef <132447715+Yo-omega@users.noreply.github.com>
2025-01-09 11:37:49 +0100

    Create main.h
    the header file for our functions

---
+++ src/main.h
@@ -0,0 +1,20 @@
+
+#ifndef MAIN_H
+
+# define MAIN_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+//structures here:
+
+//functions here:
+
+//integers & math:
+
+//return absolute value of an int, if n = MIN_INT returns (-1) for error
+//function is protected
+int    abs_(int n);
+
+#endif
